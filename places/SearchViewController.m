0a2f4ca3217c5f5f3b04dcdc58dfffc5ed556782
Adam Fraser <adamfraser0@gmail.com>
2013-03-08 16:53:43 -0500

    Add SearchViewController.

---
+++ places/SearchViewController.m
@@ -0,0 +1,17 @@
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
